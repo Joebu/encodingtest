48e76f377986932bb14fbae756cee376755096fe
yossi <yossi.buch@gmail.com>
2018-09-14 13:23:50 -0400

    init commit

---
+++ encodingtest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace encodingtest
+{
+    using static Console;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var xx = Encoding.GetEncoding(1252);
+            //WriteLine(xx.BodyName);
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var xx = Encoding.GetEncoding("Windows-1252");
+            WriteLine(xx.BodyName);
+            ReadKey();
+        }
+    }
+}
